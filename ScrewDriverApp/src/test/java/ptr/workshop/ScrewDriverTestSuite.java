22390238f2e9743a8fd89a138a47162ecc67e6bd
Piotr Sutor <60980702+Goorolx@users.noreply.github.com>
2020-07-17 15:02:47 +0200

    ScrewDriver first test

--- ScrewDriverApp/src/test/java/ptr/workshop/ScrewDriverTestSuite.java
+++ ScrewDriverApp/src/test/java/ptr/workshop/ScrewDriverTestSuite.java
@@ -1,5 +1,23 @@
+package ptr.workshop;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScrewDriverTestSuite {
+
+    ScrewDriver sd = new ScrewDriver("GreenOne",HeadType.FlatHead,100,10,
+            LocalDate.now(),LocalDate.of(2020,05,20));
+
+    @Test
+    public void shouldReturnScrewDriver(){
+        String sdName = sd.getName();
+        String ht = sd.getHeadType().name();
+
+        assertEquals("FlatHead",ht);
+        assertEquals("GreenOne",sdName);
+    }
 
 }

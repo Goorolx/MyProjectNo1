42703692f0f107bcce3f5d7ee4cffbbb5809f96a
Piotr Sutor <60980702+Goorolx@users.noreply.github.com>
2020-07-18 07:55:35 +0200

    Screw and ScrewDriver Tests - small updates and clean up

--- ScrewDriverApp/src/test/java/ptr/workshop/ScrewDriverTestSuite.java
+++ ScrewDriverApp/src/test/java/ptr/workshop/ScrewDriverTestSuite.java
@@ -16,9 +16,10 @@ class ScrewDriverTestSuite {
 
     @Test
     public void shouldReturnScrewDriver() {
+        //given & when
         String sdName = sd.getName();
         String ht = sd.getHeadType().name();
-
+        //then
         assertEquals("FlatHead", ht);
         assertEquals("GreenOne", sdName);
     }
@@ -32,31 +33,43 @@ class ScrewDriverTestSuite {
     @ParameterizedTest
     @CsvSource(value = {"2.5, 0-1", "3,2-4", "3.5,5-9", "5,10-16", "6,18-24", "8,5/8 & 3/4"})
     public void shouldReturnCorrectDiameterScrewDriver(double number, String expSizes) {
+        //given & when
         String fits = sd.checkScrewFit(number);
-
         //Then
         assertEquals(expSizes, fits);
+
+    }
+
+    @Test
+    public void shouldReturnWrongSize() {
+        //given & when
+        String fits = sd.checkScrewFit(2.7);
+        //then
+        assertEquals("Not valid Phillips tip size: please provide 2.5, 3, 3.5, 5, 6, 8", fits);
     }
 
     @Test
     public void shouldIncreaseDaysInUsage() {
+        //given & when
         sd.useScrewDriverDay(7);
         int daysU = sd.getDaysInUsage();
-
+        //then
         assertEquals(7, daysU);
     }
 
     @Test
     public void shouldAddComment() {
+        //given & when
         sd.setComment("Good Tool");
-
+        //then
         assertEquals("Good Tool", sd.getComment());
     }
 
     @Test
-    public void shouldSetGoodFalse(){
+    public void shouldSetGoodFalse() {
+        //given & when
         sd.setGood(false);
-
+        //then
         assertFalse(sd.isGood());
     }
 
--- ScrewDriverApp/src/test/java/ptr/workshop/ScrewTestSuite.java
+++ ScrewDriverApp/src/test/java/ptr/workshop/ScrewTestSuite.java
@@ -1,4 +1,6 @@
 package ptr.workshop;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,34 +13,32 @@ public class ScrewTestSuite {
 
     //Basic Tests to check main functionalities
     @Test
-    public void shouldAddNewScrew() {
+    public void shouldAddNewScrewWithCorrectParameters() {
         //given
         Screw s1 = new Screw("a12", HeadType.Phillips, 5.5, 0.5);
         //when
         String s1name = s1.getHeadType().name();
         //then
-        assertNotEquals("FlatHead",s1name);
-        assertEquals("Phillips",s1name);
-
+        assertNotEquals("FlatHead", s1name);
+        assertEquals("Phillips", s1name);
+        assertEquals(5.5, s1.getScrewLength());
+        assertEquals(0.5, s1.getScrewDiameter());
+        assertEquals("a12",s1.getName());
     }
 
     @Test
-    public void shouldReturnCorrectScrewsDiameter(){
-
+    public void shouldReturnCorrectScrewsDiameter() {
         String sName = s.getHeadType().name();
-
         System.out.println(sName);
-        //then
-       // assertEquals();
-        //assertTrue(izbecka.getScrews.notEmpty)
-
     }
 
+    @BeforeAll
+    public static void testingStartMessage() {
+        System.out.println("Starting Tests");
+    }
 
-@BeforeAll
-        public static void testingStartMessage(){
-    System.out.println("Starting Tests");
-}
-
-
+    @AfterAll
+    public static void testingEndMessage(){
+        System.out.println("Testing is done");
+    }
 }
